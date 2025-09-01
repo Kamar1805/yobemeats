b052ac2e5337413463dd92f8339edca5ced5660a
Kamar <abukamar1805@gmail.com>
2025-09-01 10:44:43 +0100

    check

--- src/data/team.js
+++ src/data/team.js
@@ -2,37 +2,38 @@ export const boardMembers = [
     {
       name: "Dr. Said Alkali Kori",
       title: "The Chairman",
-      subtitle: "Nigerian Food Corporation\nYobe Meats Damaturu Abattoir",
+      subtitle: "Nigerian Food Corporation",
       image: "https://horizons-cdn.hostinger.com/f2126a34-b61c-429a-92e5-573283323be1/3cb0ab0e64a1f2341fffbfcce1166a7e.jpg"
     },
     {
       name: "Dr. Babagana Mohammed Adam",
-      title: "Managing Director, Nigerian Food Corporation",
-      subtitle: "",
+      title: "Managing Director",
+      subtitle: "Nigerian Food Corporation",
       image: "https://horizons-cdn.hostinger.com/f2126a34-b61c-429a-92e5-573283323be1/35fdcdca9d1384569f10a46f22341058.webp"
     },
     {
       name: "Samalla Hassan Yusuf",
-      title: "Executive Director/COO, Nigerian Food Corporation",
-      subtitle: "",
+      title: "Executive Director/COO ",
+      subtitle: "Nigerian Food Corporation",
       image: "https://horizons-cdn.hostinger.com/f2126a34-b61c-429a-92e5-573283323be1/09b49e18adfc37caae61950a491766dd.png"
     },
+    
     {
-      name: "Hon. Ali Mustapha Goniri",
-      title: "Non-Executive Director, Nigerian Food Corporation",
-      subtitle: "",
-      image: "https://horizons-cdn.hostinger.com/f2126a34-b61c-429a-92e5-573283323be1/cb0b77c9e87541090981921a36822abc.jpg"
+      name: "Hussaini Tela Zimbo",
+      title: "Executive Director/CFO",
+      subtitle: " Nigerian Food Corporation",
+      image: ""
     },
     {
       name: "Dr. Aliyu Chikaji",
-      title: "Non-Executive Director, Nigerian Food Corporation",
-      subtitle: "",
+      title: "Non-Executive Director ",
+      subtitle: "Nigerian Food Corporation",
       image: "https://horizons-cdn.hostinger.com/f2126a34-b61c-429a-92e5-573283323be1/eac11d55398cd6024e417c95ec29e71b.jpg"
     },
     {
       name: "Liman Ali Ibrahim",
-      title: "Non-Executive Director, Nigerian Food Corporation",
-      subtitle: "",
+      title: "Non-Executive Director, ",
+      subtitle: "Nigerian Food Corporation",
       image: "https://horizons-cdn.hostinger.com/f2126a34-b61c-429a-92e5-573283323be1/bcf8babcf89f9f4179d5880a869c2125.png"
     },
     {
@@ -41,30 +42,15 @@ export const boardMembers = [
       subtitle: "",
       image: "https://horizons-cdn.hostinger.com/f2126a34-b61c-429a-92e5-573283323be1/e5e461cfee59151f210eec7d9cb19a6b.png"
     },
+    
     {
-      name: "Hussaini Tela Zimbo",
-      title: "Non-Executive Director, Nigerian Food Corporation",
-      subtitle: "",
-      image: ""
-    },
-    {
-      name: "Abdullahi Hassan Gana",
-      title: "Non-Executive Director, Nigerian Food Corporation",
-      subtitle: "",
-      image: "https://horizons-cdn.hostinger.com/f2126a34-b61c-429a-92e5-573283323be1/922be741358060084a06af9f813f6740.png"
-    },
-    {
-      name: "Mohammed Abatcha Geidam",
-      title: "Non-Executive Director, Nigerian Food Corporation",
-      subtitle: "",
-      image: "https://horizons-cdn.hostinger.com/f2126a34-b61c-429a-92e5-573283323be1/9f4da79b0a565ee402e481d7d5e18618.png"
-    },
-    {
-      name: "Hon. Kaigama Umar Yunusari",
-      title: "Non-Executive Director, Nigerian Food Corporation",
-      subtitle: "",
-      image: "https://horizons-cdn.hostinger.com/f2126a34-b61c-429a-92e5-573283323be1/74217401f245cebe012c6a0c4b482cfc.jpg"
+      name: "Hamza Saidu",
+      title: "Non-Executive Director ",
+      subtitle: "Nigerian Food Corporation",
+      image: null
     }
+   
+    
   ];
 
   export const managementTeam = [];
